8256b2d72b167b22bfc73b667c6cc320fb072dad
elbert timothy thomas <elberthomax@gmail.com>
2023-04-18 15:27:03 +0800

    fix ip issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: (process.env.NODE_ENV !== 'production'? 'localhost': "0.0.0.0"),
         routes: {
             cors: {
                 origin: ['http://notesapp-v1.dicodingacademy.com']
@@ -15,7 +15,6 @@ const init = async () => {
     server.route(routes)
 
     await server.start()
-    console.log(`server berjalan pada ${server.info.uri}`)
 }
 
 init()
